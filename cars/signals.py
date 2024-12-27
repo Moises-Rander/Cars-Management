020d5c6aeeb933ca17d8b5af2f9e81efee2f1614
Moises Rander <moisesrander@outlook.com>
2024-12-27 08:42:10 -0300

    car_value on delete ajustment

--- cars/signals.py
+++ cars/signals.py
@@ -24,25 +24,31 @@ def delete_car_image_on_update(sender, instance, **kwargs):
             pass
 
 
-def car_inventory_update(instance):
+def car_inventory_update():
     cars_count = Car.objects.all().count()
     cars_value = Car.objects.aggregate(
         total_value=Sum('value')
     )['total_value']
-    CarInventory.objects.create(
-        cars_count=cars_count,
-        cars_value=cars_value,
-    )
+    if cars_value:
+        CarInventory.objects.create(
+            cars_count=cars_count,
+            cars_value=cars_value,
+        )
+    else:
+        CarInventory.objects.create(
+            cars_count=cars_count,
+            cars_value=0,
+        )
 
 
 @receiver(post_save, sender=Car)
 def increment_car_count_on_create(sender, instance, **kwargs):
-    car_inventory_update(instance)
+    car_inventory_update()
 
 
 @receiver(post_delete, sender=Car)
 def decrement_car_count_on_delete(sender, instance, **kwargs):
-    car_inventory_update(instance)
+    car_inventory_update()
 
 
 @receiver(pre_save, sender=Car)
